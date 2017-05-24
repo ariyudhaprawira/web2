6ace1b32eab853455733d714980f458579e3cae3
ariyudhaprawira <ariyudhaprawira@gmail.com>
2017-05-24 15:18:18 +0800

    Menampilkan Halaman Depan dari Database

--- administrator/pages/beranda.php
+++ administrator/pages/beranda.php
@@ -1,3 +1,9 @@
                         <h1 class="page-header">Beranda</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nemo velit voluptas vero dolor, ad sint voluptatum aperiam, ratione accusantium numquam. Iusto nobis, quam doloribus consequuntur veritatis officia voluptates autem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, quasi sequi. Id odio qui, ab mollitia neque fugit officiis, inventore omnis, laborum repellat cumque. Vero tempora assumenda asperiores tenetur labore?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nemo velit voluptas vero dolor, ad sint voluptatum aperiam, ratione accusantium numquam. Iusto nobis, quam doloribus consequuntur veritatis officia voluptates autem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, quasi sequi. Id odio qui, ab mollitia neque fugit officiis, inventore omnis, laborum repellat cumque. Vero tempora assumenda asperiores tenetur labore?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
              
--- view/UtamaUI.php
+++ view/UtamaUI.php
@@ -2,16 +2,20 @@
 
 	require_once 'View.php';
 
+
 	class UtamaUI extends View
 	{
 		
-		public function tampilBerita();
+		public function tampilBerita()
 		{
+		include_once 'model/Berita.php';
+
+		$brt = new Berita();
+
+		$isi_berita = $brt->ambilBerita();
 			include_once 'pages/beranda.php';
 			$this->end();
 		}
 	}
-
-
 
  ?>
